36ab30020e37655942cb1b875181d9e8de4fdc4d
kimhyonseong <corea6524@gmail.com>
2020-04-06 22:25:40 +0900

    db error

--- basic/admin/lib/db.php
+++ basic/admin/lib/db.php
@@ -1,14 +1,49 @@
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'].'config.php';
+//include_once $_SERVER['DOCUMENT_ROOT'].'/config.php';
 
 class db
 {
     public $conn = null;
+    public $DB1;
 
-    public function __construct()
+    public function __construct($DB1 = array())
     {
-        $pdo = new PDO('mysql:host='.$DB1['Host'].'dbname='.$DB1['dbName'],$DB1['dbUser'],$DB1['dbPass']);
-        $this->conn = $pdo;
+        try {
+            $pdo = new PDO('mysql:host=' . $DB1['Host'] . 'dbname=' . $DB1['dbName'], $DB1['dbUser'], $DB1['dbPass']);
+            $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $this->conn = $pdo;
+            var_dump($DB1['Host']);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function insert_user($id,$pw,$name,$birth){
+
+        try {
+            $dbi = $this->conn;
+            //var_dump($dbi);
+
+            $param = array(
+                ':id' => $id,
+                ':pw' => $pw,
+                ':name' => $name,
+                ':birth' => $birth
+            );
+            $query = 'insert into user(id,pw,name,birth)
+                  values(":id",":pw",":name",":birth")';
+
+//            $stmt = $dbi->prepare($query);
+//
+//            foreach ($param as $key => $val) {
+//                $stmt->bindParam($key, $val);
+//            }
+//            $stmt->execute();
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+
     }
 
 }
--- basic/admin/proc/signup.php
+++ basic/admin/proc/signup.php
@@ -1,1 +1,12 @@
 <?php
+    include_once $_SERVER['DOCUMENT_ROOT'].'/config.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/lib/db.php';
+
+    $id = $_POST['id'];
+    $pw = $_POST['pw'];
+    $name = $_POST['name'];
+    $birth = $_POST['birth'];
+
+    var_dump($DB1);
+    $dbi = new db($DB1);
+    $dbi->insert_user($id,$pw,$name,$birth);
