571072f1535029471adf371b4ba87ee67b4300ef
kimhyonseong <corea6524@gmail.com>
2020-10-28 00:11:23 +0900

    DB seed

--- laravel5/database/seeders/DatabaseSeeder.php
+++ laravel5/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+//use App\Models;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +19,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        //Models::unguard();
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        $this->call(UsersTableSeeder::class);
+        $this->command->info('users table seeded');
+
+        $this->call(PostsTableSeeder::class);
+        $this->command->info('posts table seeded');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        //Models::reguard();
     }
 }
