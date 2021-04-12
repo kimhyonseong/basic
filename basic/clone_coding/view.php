cd6ec950c2f714248d9bbffba813c6673a007a60
kimhyonseong <corea6524@gmail.com>
2021-04-12 22:31:34 +0900

    basic markup

---
+++ basic/clone_coding/view.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/8bb6ae7263.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="view.css">
+    <script src="view.js"></script>
+    <title>clone Coding - Youtube mobile</title>
+</head>
+<body>
+<header class="nav_box">
+    <div class="nav_log">
+        <i class="fab fa-youtube"></i>
+        <a href="javascript:void(0);">Youtube</a>
+    </div>
+    <div class="nav_etc">
+        <i class="fas fa-search"></i>
+        <i class="fas fa-ellipsis-v"></i>
+    </div>
+</header>
+<section>
+    <article class="video_div">
+        <video class="video" controls></video>
+        <div class="video_title">
+            Clone Coding: Youtube Mobile Website 유튜브 모바일 웹사이트 따라 만들기
+            | 프론트앤드 개발자 입문편 : HTML, CSS, Javascript
+        </div>
+        <div class="reg_date">
+            1M views 1 month ago
+        </div>
+        <ul class="evaluate">
+            <li><i class="fas fa-thumbs-up"></i><span>1K</span></li>
+            <li><i class="fas fa-thumbs-up fa-flip-vertical"></i><span>0</span></li>
+            <li><i class="fas fa-share"></i><span>Share</span></li>
+            <li><i class="fas fa-plus"></i><span>Save</span></li>
+            <li><i class="fas fa-flag"></i><span>Report</span></li>
+        </ul>
+    </article>
+</section>
+<section>
+    <ul>
+        <li>
+            <img src="" alt="">
+            <div class="title"></div>
+            <div class="desc"></div>
+            <button></button>
+        </li>
+        <li><img src="" alt="">
+            <div class="title"></div>
+            <div class="desc"></div>
+            <button></button></li>
+        <li><img src="" alt="">
+            <div class="title"></div>
+            <div class="desc"></div>
+            <button></button></li>
+        <li><img src="" alt="">
+            <div class="title"></div>
+            <div class="desc"></div>
+            <button></button></li>
+        <li><img src="" alt="">
+            <div class="title"></div>
+            <div class="desc"></div>
+            <button></button></li>
+    </ul>
+</section>
+</body>
+</html>
