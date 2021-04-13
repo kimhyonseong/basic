31a29c25f35cba786d45692caf938ed64e61e347
kimhyonseong <corea6524@gmail.com>
2021-04-13 21:34:36 +0900

    html modify

--- basic/clone_coding/view.php
+++ basic/clone_coding/view.php
@@ -11,59 +11,104 @@
     <title>clone Coding - Youtube mobile</title>
 </head>
 <body>
-<header class="nav_box">
-    <div class="nav_log">
+<!-- header -->
+<header>
+    <div class="logo">
         <i class="fab fa-youtube"></i>
-        <a href="javascript:void(0);">Youtube</a>
+        <span class="title">Youtube</span>
     </div>
-    <div class="nav_etc">
+    <div class="icon">
         <i class="fas fa-search"></i>
         <i class="fas fa-ellipsis-v"></i>
     </div>
 </header>
-<section>
-    <article class="video_div">
-        <video class="video" controls></video>
-        <div class="video_title">
-            Clone Coding: Youtube Mobile Website 유튜브 모바일 웹사이트 따라 만들기
-            | 프론트앤드 개발자 입문편 : HTML, CSS, Javascript
-        </div>
-        <div class="reg_date">
-            1M views 1 month ago
-        </div>
-        <ul class="evaluate">
-            <li><i class="fas fa-thumbs-up"></i><span>1K</span></li>
-            <li><i class="fas fa-thumbs-up fa-flip-vertical"></i><span>0</span></li>
-            <li><i class="fas fa-share"></i><span>Share</span></li>
-            <li><i class="fas fa-plus"></i><span>Save</span></li>
-            <li><i class="fas fa-flag"></i><span>Report</span></li>
-        </ul>
-    </article>
+
+<!-- video player -->
+<section class="player">
+    <video src="" class="video" controls></video>
 </section>
-<section>
-    <ul>
+
+<!-- video information -->
+<section class="info">
+    <div class="metadata">
+        <ul class="hashTag">
+            <li>#HTML</li>
+            <li>#CSS</li>
+            <li>#coding</li>
+        </ul>
+        <div class="titleAndButton">
+            <span class="title">Clone Coding: Youtube Mobile Website 유튜브 모바일 웹사이트 따라 만들기
+                | 프론트앤드 개발자 입문편 : HTML, CSS, Javascript</span>
+            <button class="moreBt">
+                <i class="fas fa-caret-down"></i>
+            </button>
+        </div>
+        <div class="views">
+            <span class="views">1M views 1 month ago</span>
+        </div>
+    </div>
+    <ul class="actions">
         <li>
-            <img src="" alt="">
-            <div class="title"></div>
-            <div class="desc"></div>
-            <button></button>
+            <button>
+                <i class="fas fa-thumbs-up"></i><span>1K</span>
+            </button>
         </li>
-        <li><img src="" alt="">
-            <div class="title"></div>
-            <div class="desc"></div>
-            <button></button></li>
-        <li><img src="" alt="">
-            <div class="title"></div>
-            <div class="desc"></div>
-            <button></button></li>
-        <li><img src="" alt="">
-            <div class="title"></div>
-            <div class="desc"></div>
-            <button></button></li>
-        <li><img src="" alt="">
-            <div class="title"></div>
-            <div class="desc"></div>
-            <button></button></li>
+        <li>
+            <button>
+                <i class="fas fa-thumbs-up fa-flip-vertical"></i><span>0</span>
+            </button>
+        </li>
+        <li>
+            <button><i class="fas fa-share"></i><span>Share</span></button>
+        </li>
+        <li>
+            <button><i class="fas fa-plus"></i><span>Save</span></button>
+        </li>
+        <li>
+            <button><i class="fas fa-flag"></i><span>Report</span></button>
+        </li>
+    </ul>
+    <div class="channel">
+        <div class="metadata">
+            <img src="" alt="">
+            <div class="info">
+                <span class="name">파란햇님</span>
+                <span class="subscribers">20 subscribers</span>
+            </div>
+        </div>
+        <button class="subscribe">subscribe</button>
+    </div>
+</section>
+<section class="others_video">
+    <span class="title">Up Next</span>
+    <ul>
+        <li class="item">
+            <img src="https://via.placeholder.com/270x150" alt="">
+            <div class="info">
+                <span class="title"></span>
+                <span class="name"></span>
+                <span class="views"></span>
+            </div>
+            <button class="more"></button>
+        </li>
+        <li class="item">
+            <img src="https://via.placeholder.com/270x150" alt="">
+            <div class="info">
+                <span class="title"></span>
+                <span class="name"></span>
+                <span class="views"></span>
+            </div>
+            <button class="more"></button>
+        </li>
+        <li class="item">
+            <img src="https://via.placeholder.com/270x150" alt="">
+            <div class="info">
+                <span class="title"></span>
+                <span class="name"></span>
+                <span class="views"></span>
+            </div>
+            <button class="more"></button>
+        </li>
     </ul>
 </section>
 </body>
