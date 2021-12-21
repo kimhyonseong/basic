775d78649a7a277c28a26c5cdc18ee1f412e2d6f
kimhyonseong <corea6524@gmail.com>
2021-12-21 22:12:23 +0900

    ch5-8~13

---
+++ java_base/src/array/array6.java
@@ -0,0 +1,19 @@
+package array;
+
+import java.util.Arrays;
+
+public class array6 {
+    public static void main(String[] args) {
+        int[] numArr = {0,1,2,3,4,5,6,7,8,9};
+        System.out.println(Arrays.toString(numArr));
+
+        for (int i=0; i<numArr.length; i++) {
+            int n = (int)(Math.random() * 10);
+            int tmp = numArr[i];
+            numArr[i] = numArr[n];
+            numArr[n] = tmp;
+        }
+
+        System.out.println(Arrays.toString(numArr));
+    }
+}
