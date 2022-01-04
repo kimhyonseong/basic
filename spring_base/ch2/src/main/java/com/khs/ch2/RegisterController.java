57d6527655a3e55af463f430c999aa89a7714fa4
kimhyonseong <corea6524@gmail.com>
2022-01-04 22:27:41 +0900

    registerForm

--- spring_base/ch2/src/main/java/com/khs/ch2/RegisterController.java
+++ spring_base/ch2/src/main/java/com/khs/ch2/RegisterController.java
@@ -1,16 +1,21 @@
 package com.khs.ch2;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // ctrl + shift + o 자동 임포트 
 public class RegisterController {
-	@RequestMapping("/register/add")
-	public String register() {
-		return "registerForm";
-	}
+	//@RequestMapping(value="/register/save", method=RequestMethod.POST)
+	//@RequestMapping("/register/add")
+//	servlet-context.xml에 view-controller 추가
+//	@GetMapping("/register/add")
+//	public String register() {
+//		return "registerForm";
+//	}
 	
-	@RequestMapping("/register/save")
+	//@RequestMapping(value="/register/save", method=RequestMethod.POST)
+	@PostMapping("/register/save")
 	public String save() {
 		return "registerInfo";
 	}
